9f0d001e06b46ae5a092297d55675238eb1f55e1
Blair <blairr1@seattleu.edu>
2019-04-15 20:41:12 -0700

    test

--- empdata.js
+++ empdata.js
@@ -55,6 +55,70 @@
       "Age": "24",
       "Hometown": "Denver",
       "Favorite Food": "Chicken Fried Steak"
+    },
+    {
+      "name": "Jake Feeder",
+      "Job Type": "Soccer Coach",
+      "Salary": "$44000",
+      "Age": "25",
+      "Hometown": "Los Vegas",
+      "Favorite Food": "Pasta"
+    },
+    {
+      "name": "Pete Fiers",
+      "Job Type": "Data Scientist",
+      "Salary": "$98000",
+      "Age": "52",
+      "Hometown": "Phoenix",
+      "Favorite Food": "Chicken Nuggets"
+    },
+    {
+      "name": "Paul Weir",
+      "Job Type": "Basketball Coach",
+      "Salary": "$2000000",
+      "Age": "45",
+      "Hometown": "Austin",
+      "Favorite Food": "Oysters"
+    },
+    {
+      "name": "Frank Neal",
+      "Job Type": "Mechanic",
+      "Salary": "$58000",
+      "Age": "19",
+      "Hometown": "New York",
+      "Favorite Food": "Hot Dog"
+    },
+    {
+      "name": "Mary Blank",
+      "Job Type": "Retired",
+      "Salary": "$0",
+      "Age": "62",
+      "Hometown": "Atlanta",
+      "Favorite Food": "Fried Chicken"
+    },
+    {
+      "name": "John Hart",
+      "Job Type": "Construction Worker",
+      "Salary": "$49000",
+      "Age": "42",
+      "Hometown": "Chicago",
+      "Favorite Food": "Deep Dish Pizza"
+    },
+    {
+      "name": "Burt Earnie",
+      "Job Type": "Comedian",
+      "Salary": "$40000",
+      "Age": "68",
+      "Hometown": "Miami",
+      "Favorite Food": "Club Sandwich"
+    },
+    {
+      "name": "Rylee Noodle",
+      "Job Type": "Nujrse",
+      "Salary": "$80000",
+      "Age": "30",
+      "Hometown": "Washington D.C",
+      "Favorite Food": "Toast"
     }
   ]
 }

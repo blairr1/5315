ea2b194a5ca9c3027a72129c240962a31bc15d3c
Blair <blairr1@seattleu.edu>
2019-04-15 20:32:21 -0700

    Testing

--- empdata.js
+++ empdata.js
@@ -31,6 +31,30 @@
       "Age": "30",
       "Hometown": "Portland",
       "Favorite Food": "Pizza"
+    },
+    {
+      "name": "Marshal Kosta",
+      "Job Type": "Accountant",
+      "Salary": "$74000",
+      "Age": "29",
+      "Hometown": "Boise",
+      "Favorite Food": "Sushi"
+    },
+    {
+      "name": "Brandon Hanks",
+      "Job Type": "IT Support",
+      "Salary": "$186000",
+      "Age": "35",
+      "Hometown": "Sacramento",
+      "Favorite Food": "PB&J"
+    },
+    {
+      "name": "Dan Manly",
+      "Job Type": "Golf Pro",
+      "Salary": "$2360000",
+      "Age": "24",
+      "Hometown": "Denver",
+      "Favorite Food": "Chicken Fried Steak"
     }
   ]
 }
